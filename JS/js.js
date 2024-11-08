ef8a136f0ebb1aa3dc4193d5cc4bdb2e5f83ce54
MR.DB <486196148@giza1.moe.edu.eg>
2024-11-09 00:57:26 +0200

    to-up

--- JS/js.js
+++ JS/js.js
@@ -1,6 +1,6 @@
 let skills = document.getElementsByClassName("skills")[0]
 let skillsContainer = document.getElementsByClassName("skills-container")[0]
-
+let button = document.getElementsByClassName("to-top")[0]
 
 window.onscroll = function (e){
     if (window.scrollY >= skills.offsetTop - 250) {
@@ -9,6 +9,15 @@ window.onscroll = function (e){
             element.children[1].lastElementChild.style.width = `${attr}%`
         });
     }
+    if(window.scrollY >= (window.outerHeight - 400) / 2){
+        button.style.display = "block";
+    }
+    else{
+        button.style.display = "none";
+    }
+}
+button.onclick = function(e){
+    window.scrollTo({top:0,behavior:"smooth"})
 }
 
 
