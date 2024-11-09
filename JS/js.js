42cefa0a991526f1b30f3ae4b0a97faf6d345a46
MR.DB <486196148@giza1.moe.edu.eg>
2024-11-09 19:33:27 +0200

    ok

--- JS/js.js
+++ JS/js.js
@@ -6,22 +6,29 @@ let skillsContainer = document.getElementsByClassName("skills-container")[0];
 let button = document.getElementsByClassName("to-top")[0];
 
 window.onscroll = function (e){
+    let handler1;
+    let handler2;
     if (window.scrollY >= skills.offsetTop - 250) {
         Array.from(skillsContainer.children).forEach(element => {
             let attr = parseInt(element.children[1].getAttribute("skill-progress"))
             element.children[1].lastElementChild.style.width = `${attr}%`
         });
     }
+    else{
+        Array.from(skillsContainer.children).forEach(element => {
+            element.children[1].lastElementChild.style.width = `0%`
+        });
+    }
     if(window.scrollY >= statsContainer.offsetTop - 250){
-        let handler = setInterval(function(){
+        let handler1 = setInterval(function(){
             stats[0].getAttribute("data-set") != stats[0].innerText ? stats[0].innerText++ : stats[0].innerText = stats[0].innerText;
             stats[1].getAttribute("data-set") != stats[1].innerText ? stats[1].innerText++ : stats[1].innerText = stats[1].innerText;
             stats[2].getAttribute("data-set") != stats[2].innerText ? stats[2].innerText++ : stats[2].innerText = stats[2].innerText;
             stats[3].getAttribute("data-set") != stats[3].innerText ? stats[3].innerText++ : stats[3].innerText = stats[3].innerText;
             if(stats[0].getAttribute("data-set") == stats[0].innerText && stats[1].getAttribute("data-set") == stats[1].innerText && stats[2].getAttribute("data-set") == stats[2].innerText && stats[3].getAttribute("data-set") == stats[3].innerText ){
-                clearInterval(handler)
+                clearInterval(handler1)
             }
-        },100)
+        },50)
     }
     if(window.scrollY >= (window.outerHeight - 400) / 2){
         button.style.display = "block";
