a352e61cbdf6ba28e433cf02bfe8e65c0605799b
MR.DB <486196148@giza1.moe.edu.eg>
2024-11-09 12:43:13 +0200

    stats

--- JS/js.js
+++ JS/js.js
@@ -1,6 +1,9 @@
-let skills = document.getElementsByClassName("skills")[0]
-let skillsContainer = document.getElementsByClassName("skills-container")[0]
-let button = document.getElementsByClassName("to-top")[0]
+let skills = document.getElementsByClassName("skills")[0];
+let statsContainer = document.getElementsByClassName("stats")[0];
+let stats = document.getElementsByClassName("num")
+console.log(stats)
+let skillsContainer = document.getElementsByClassName("skills-container")[0];
+let button = document.getElementsByClassName("to-top")[0];
 
 window.onscroll = function (e){
     if (window.scrollY >= skills.offsetTop - 250) {
@@ -8,6 +11,17 @@ window.onscroll = function (e){
             let attr = parseInt(element.children[1].getAttribute("skill-progress"))
             element.children[1].lastElementChild.style.width = `${attr}%`
         });
+    }
+    if(window.scrollY >= statsContainer.offsetTop - 250){
+        let handler = setInterval(function(){
+            stats[0].getAttribute("data-set") != stats[0].innerText ? stats[0].innerText++ : stats[0].innerText = stats[0].innerText;
+            stats[1].getAttribute("data-set") != stats[1].innerText ? stats[1].innerText++ : stats[1].innerText = stats[1].innerText;
+            stats[2].getAttribute("data-set") != stats[2].innerText ? stats[2].innerText++ : stats[2].innerText = stats[2].innerText;
+            stats[3].getAttribute("data-set") != stats[3].innerText ? stats[3].innerText++ : stats[3].innerText = stats[3].innerText;
+            if(stats[0].getAttribute("data-set") == stats[0].innerText && stats[1].getAttribute("data-set") == stats[1].innerText && stats[2].getAttribute("data-set") == stats[2].innerText && stats[3].getAttribute("data-set") == stats[3].innerText ){
+                clearInterval(handler)
+            }
+        },100)
     }
     if(window.scrollY >= (window.outerHeight - 400) / 2){
         button.style.display = "block";
